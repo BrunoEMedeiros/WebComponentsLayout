e64a805ab464922c7a21dccce5b90856dfd6b090
D3adSec <bruno.emedeiros99@gmail.com>
2023-09-29 13:26:02 -0300

    29/09 estilizada

--- src/Cards.ts
+++ src/Cards.ts
@@ -4,12 +4,17 @@ export class Cards extends HTMLElement{
     constructor(){
         super();
         const shadow = this.attachShadow({mode : "open"});
+        const style = document.createElement("link");
+        style.rel = "stylesheet";
+        style.href = "./cards_style.css"
+        shadow.append(style);
         this.build(shadow);
     }
 
     build(shadow: ShadowRoot){
         lista_objetos.map((produto)=>{
             const div = document.createElement("div");
+            div.className = "cards";
             const h1 = document.createElement("h1");
             const h3 = document.createElement("h3");
 
